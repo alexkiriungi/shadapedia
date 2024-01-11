0b2eb95330863eb0d08dab6067c80f913c1c6fec
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-11 16:15:54 +0300

    Authentication complete

--- controllers/auth.js
+++ controllers/auth.js
@@ -50,7 +50,7 @@ export const login = async (req, res) => {
 
         const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
         delete user.password;
-        res.status(200).json({ token: user });
+        res.status(200).json({ token, user });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
