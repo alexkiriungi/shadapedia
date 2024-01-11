60da76d6fec76d052d5b7badb5c55396eb8a3912
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-11 15:57:39 +0300

    Added unsplash images to assets folder

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,3 +3,37 @@ import jwt from 'jsonwebtoken';
 import User from './models/User.js';
 
 //REGISTER USER
+export const register = async (req, res) => {
+    try {
+        const {
+            firstName,
+            lastname,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordhash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName,
+            lastname,
+            email,
+            password: passwordhash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000),
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
