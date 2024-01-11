2122300b772605ec40d7028c9c02a584dd73526d
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-11 13:52:03 +0300

    COnfiguration and File storage

--- index.js
+++ index.js
@@ -4,3 +4,32 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url'; // proper setup paths 
+
+// CONFIGURATIONS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+// FILE STORAGE - SAVING File
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({ storage });
