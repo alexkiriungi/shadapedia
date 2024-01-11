4eb290c780bb1c03b91fd77d1b41b65f471b5f28
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-11 23:28:00 +0300

    POST model addded

---
+++ controllers/posts.js
@@ -0,0 +1,10 @@
+import Post from '../models/Post.js';
+
+//CREATE FUNCTION
+export const createPost = async (req, res) => {
+    try{
+
+    } catch (err) {
+        res.status(409).json({ message: er.message});
+    }
+}
--- routes/posts.js
+++ routes/posts.js
@@ -10,3 +10,5 @@ router.get("/:userId/posts", verifyToken, getUserPosts);
 
 //UPDATE
 router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
