56a489f1fbbac7724756786a23bfd34138ab033a
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-11 22:12:39 +0300

    added posts routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,3 +2,8 @@ import express from 'express';
 import { getFeedPosts, getUserPosts, likePost } from '../controllers/posts.js';
 import { verifyToken } from '../middleware/auth.js';
 
+const router = express.Router();
+
+//READ - curated feeds
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
