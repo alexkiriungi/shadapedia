519899716101e556ffcb0748959c20c3bf26d377
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-12 18:13:52 +0300

    Added the NavBar CSS elements and components

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -33,7 +33,110 @@ const Navbar = () => {
     const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
 
     const theme = useTheme();
-    return (<div>navbar</div>)
-}
+    const neutralLight = theme.palette.neutral.light;
+    const dark = theme.palette.neutral.dark;
+    const background = theme.palette.background.default;
+    const primaryLight = theme.palette.primary.light;
+    const alt = theme.palette.background.alt;
+
+    const fullName = `${user.firstName} ${user.lastName}`;
+
+    return (
+        <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+            <FlexBetween gap="1.75rem">
+                <Typography
+                fontWeight="bold"
+                fontSize="clamp(1rem, 2rem, 2.25rem)"
+                color="primary"
+                onClick={() => navigate("/home")}
+                sx={{
+                    "&:hover": {
+                        color: primaryLight,
+                        cursor: "pointer",
+                    },
+                }}>
+                    Shadapedia
+                </Typography>
+                {isNonMobileScreens && (
+                    <FlexBetween backgroundColor={neutralLight} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem">
+                        <InputBase placeholder="Search..." />
+                        <IconButton>
+                            <Search />
+                        </IconButton>
+                    </FlexBetween>
+                )}
+            </FlexBetween>
+
+            {/* DESKTOP NAVBAR */}
+            {isNonMobileScreens ? (
+            <FlexBetween gap="2rem">
+                <IconButton onClick={() => dispatch(setMode())}>
+                    {theme.palette.mode === "dark" ? (
+                        <DarkMode sx={{ fontSize: "25px"}} />
+                    ) : (
+                        <LightMode sx={{ color:dark, fontSize: "25px"}} />
+                    )}
+                </IconButton>
+                <Message sx={{ fontSize: "25px"}} />
+                <Notifications sx={{ fontSize: "25px"}} />
+                <Help sx={{ fontSize: "25px"}} />
+                <FormControl variant="standard" value={fullName}>
+                    <Select
+                    value={fullname}
+                    sx={{
+                        backgroundColor: neutralLight,
+                        width: "150px",
+                        borderRadius: "0.25rem",
+                        padding: "0.25rem 1rem",
+                        "& .MuiSvgIcon-root": {
+                            paddingRight: "0.25rem",
+                            width: "3rem"
+                        },
+                        "& .MuiSelect-select:focus": {
+                            backgroundColor: neutralLight
+                        }
+                    }}
+                    input={<InputBase />}
+                    >
+                        <MenuItem value={fullName}>
+                            <Typography>{fullName}</Typography>
+                        </MenuItem>
+                        <MenuItem onClick={() => dispatch(setLogout())}>Log Out</MenuItem>
+                    </Select>
+                </FormControl>
+            </FlexBetween>
+            ) : (
+            <IconButton
+                onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+            >
+                <Menu />
+            </IconButton>
+            )}
+
+            {/* MOBILE NAVBAR */}
+            {!isNonMobileScreens && isMobileMenuToggled && (
+                <Box
+                    position="fixed"
+                    right="0"
+                    bottom="0"
+                    height="100%"
+                    zIndex= "10"
+                    maxWidth="500px"
+                    minWidth="300px"
+                    backgroundColor={background}
+                >
+                    {/* CLOSE ICON */}
+                    <Box display="flex" justifyContent="flex-end" padding="1rem">
+                        <IconButton
+                            onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+                        >
+                            <Close />
+                        </IconButton>
+                    </Box>
+                </Box>
+            )}
+        </FlexBetween>
+    );
+};
 
 export default Navbar;
