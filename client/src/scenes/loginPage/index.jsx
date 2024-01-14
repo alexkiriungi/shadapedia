86514debadc5d0388729793e75ae8b44f617a552
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-14 13:58:49 +0300

    Login page/register page form setup

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,6 +1,36 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 
 const LoginPage = () => {
-    return (<div>loginpage</div>)
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    return <Box>
+        <Box width="100%" 
+        backgroundColor={theme.palette.background.alt}
+        padding="1rem 6%"
+        textAlign="center"
+        >
+            <Typography
+                fontWeight="bold"
+                fontSize="32px"
+                color="primary"
+            >
+                Shadapedia
+            </Typography>
+        </Box>
+
+        <Box
+            width={isNonMobileScreens ? "50%" : "93% "}
+            padding="2rem"
+            margin="2rem auto"
+            borderRadius="1.5rem"
+            backgroundColor= {theme.palette.background.alt}
+        >
+            <Typography fontWeight="500" variant="h5" sx={{ marginBottom: "1.5rem"}}>
+                Welcome to Shadapedia, the Social Media for Weirdos!
+            </Typography>
+
+        </Box>
+    </Box>
 };
 
 export default LoginPage;
