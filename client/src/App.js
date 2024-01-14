a1218dfec7e4ba2d0c9b3afbd49382c85bc50041
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-14 23:57:24 +0300

    Added Widget Wrapper and UserImage components

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
   
   return (
     <div className="app">
@@ -19,8 +20,8 @@ function App() {
           <CssBaseline /> 
           <Routes>
             <Route path="/" element= {<LoginPage />} />
-            <Route path="/home" element= {<HomePage />} />
-            <Route path="/profile/:userId" element= {<ProfilePage />} />
+            <Route path="/home" element= {isAuth ? <HomePage /> : <Navigate to="/" />} />
+            <Route path="/profile/:userId" element= {isAuth ? <ProfilePage /> : <Navigate to="/" />} />
         </Routes>
         </ThemeProvider>
       </BrowserRouter>
