3019476d805c230525112ed5df453125f9389700
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-15 09:12:32 +0300

    MyPostWidget completed

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -28,4 +28,143 @@ import { setPosts } from "state";
 const MyPostWidget = ({ picturePath }) => {
     const dispatch = useDispatch();
     const [isImage, setIsImage] = useState(false);
-}
+    const [image, setImage] = useState(null);
+    const [post, setPost] = useState("");
+    const { palette } = useTheme();
+    const { _id } = useSelector((state) => state.user);
+    const token = useSelector((state) => state.token);
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    const mediumMain = palette.neutral.mediumMain;
+    const medium = palette.neutral.medium;
+
+    const handlePosts = async () => {
+        const formData = new FormData();
+        formData.append("userid", _id);
+        formData.append("description", post);
+        if (image) {
+            formData.appaned("picture", image);
+            formData.append("picturePath", image.name);
+        }
+
+        const response = await fetch(`http://localhost:3001/posts`, {
+            method: "POST",
+            headers: { Authorization: `Bearer ${token}`},
+            body: formData,
+        });
+        const posts = await response.json();
+        dispatch(setPosts({ posts }));
+        setImage(null);
+        setPost("");
+    };
+
+    return (
+        <WidgetWrapper>
+            <FlexBetween gap="1.5rem">
+                <UserImage image={picturePath} />
+                <InputBase 
+                    placeholder="What's on your mind Today..."
+                    onChange={(e) => setPosts(e.target.value)}
+                    value={post}
+                    sx={{
+                        width: "100%",
+                        backgroundColor: palette.neutral.light,
+                        borderRadius: "2rem",
+                        padding: "1rem 2rem"
+                    }}
+                />
+            </FlexBetween>
+            {isImage && (
+                <Box
+                    border={`1px solid ${medium}`}
+                    borderRadius="5px"
+                    marginTop="1rem"
+                    padding="1rem"
+                >
+                    <Dropzone
+                        acceptedFiles=".jpg,.jpeg,.png"
+                        multiple={false}
+                        onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}
+                    >
+                        {({ getRootProps, getInputProps }) => (
+                            <FlexBetween>
+                                <Box
+                                    {...getRootProps()}
+                                    border={`2px dashed ${palette.primary.main}`}
+                                    padding="1rem"
+                                    width="100%"
+                                    sx={{ "&:hover": { cursor: "pointer" }}}
+                                >
+                                    <input {...getInputProps()} />
+                                    {!image ? (
+                                        <p>Add image Here</p>
+                                    ) : (
+                                        <FlexBetween>
+                                            <Typography>{image.name}</Typography>
+                                            <EditOutlined />
+                                        </FlexBetween>
+                                    )}
+                                </Box>
+                                {image && (
+                                    <IconButton
+                                        onClick={()=> setImage(null)}
+                                        sx={{ width: "15%" }}
+                                    >
+                                        <DeleteOutlined />
+                                    </IconButton>
+                                )}
+                            </FlexBetween>
+                        )}
+                    </Dropzone>
+                </Box>
+            )}
+
+            <Divider sx={{ margin: "1.25rem 0" }} />
+
+            <FlexBetween>
+                <FlexBetween gap="0.25rem" onClick={() => setImage(!isImage)}>
+                    <ImageOutlined sx={{ color: mediumMain }} />
+                    <Typography
+                        color={mediumMain}
+                        sx={{ "&:hover": { cursor: "pointer", color: medium }}}
+                    >
+                        Image
+                    </Typography>
+                </FlexBetween>
+
+                {isNonMobileScreens ? (
+                    <>
+                        <FlexBetween gap="0.25rem">
+                            <GifBoxOutlined sx={{ color: mediumMain }} />
+                            <Typography color={mediumMain}>Clip</Typography>
+                        </FlexBetween>
+
+                        <FlexBetween gap="0.25rem">
+                            <AttachFileOutlined sx={{ color: mediumMain }} />
+                            <Typography color={mediumMain}>Attachment</Typography>
+                        </FlexBetween>
+
+                        <FlexBetween gap="0.25rem">
+                            <MicOutlined sx={{ color: mediumMain }} />
+                            <Typography color={mediumMain}>Audio</Typography>
+                        </FlexBetween>
+                    </>
+                ) : (<FlexBetween gap="0.25rem">
+                        <MoreHorizOutlied sx={{ color: mediumMain }} />
+                    </FlexBetween>
+                    )}
+
+                    <Button
+                        disabled={!post}
+                        onClick={handlePosts}
+                        sx={{
+                            color: palette.background.alt,
+                            backgroundColor: palette.primary.main,
+                            borderRadius: "3rem"
+                        }}
+                    >
+                        POST
+                    </Button>
+            </FlexBetween>
+        </WidgetWrapper>
+    )
+};
