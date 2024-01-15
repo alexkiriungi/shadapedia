8cb3951b45949c797aacffda9b8afc12f9d4aa41
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-01-15 08:22:14 +0300

    Fixed multiple imports for MyPostWidget

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -1,3 +1,31 @@
 import {
     EditOutlined,
+    DeleteOutlined,
+    AttachFileOutlined,
+    GifBoxOutlined,
+    ImageOutlined,
+    MicOutlined,
+    MoreHorizOutlied,
+} from "@mui/icons-material";
+import { 
+    Box, 
+    Divider, 
+    Typography,
+    InputBase,
+    useTheme,
+    Button,
+    IconButton,
+    useMediaQuery,
+} from "@mui/material";
+import Dropzone from "react-dropzone";
+import FlexBetween from "components/FlexBetween";
+import UserImage from "components/UserImage";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
+
+const MyPostWidget = ({ picturePath }) => {
+    const dispatch = useDispatch();
+    const [isImage, setIsImage] = useState(false);
 }
