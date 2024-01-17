bdd6981829e7b98c1b43824c9687bb476cb51ebb
alexkiriungi <mwasxander@gmail.com>
2024-01-18 00:10:28 +0300

    modified PostWidget.jsx

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -1,20 +1,20 @@
-import { 
+import {
     ChatBubbleOutlineOutlined,
     FavoriteBorderOutlined,
     FavoriteOutlined,
     ShareOutlined,
-} from "@mui/icons-material";
-import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
-import FlexBetween from "components/FlexBetween";
-import Friend from "components/Friend";
-import WidgetWrapper from "components/WidgetWrapper";
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setPost } from "state";
-
-const PostWidget = ({
+  } from "@mui/icons-material";
+  import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+  import FlexBetween from "components/FlexBetween";
+  import Friend from "components/Friend";
+  import WidgetWrapper from "components/WidgetWrapper";
+  import { useState } from "react";
+  import { useDispatch, useSelector } from "react-redux";
+  import { setPost } from "state";
+  
+  const PostWidget = ({
     postId,
-    postuserId,
+    postUserId,
     name,
     description,
     location,
@@ -22,89 +22,92 @@ const PostWidget = ({
     userPicturePath,
     likes,
     comments,
-}) => {
+  }) => {
     const [isComments, setIsComments] = useState(false);
     const dispatch = useDispatch();
     const token = useSelector((state) => state.token);
     const loggedInUserId = useSelector((state) => state.user._id);
     const isLiked = Boolean(likes[loggedInUserId]);
-    const likesCount = Object.keys(likes).length;
-
+    const likeCount = Object.keys(likes).length;
+  
     const { palette } = useTheme();
     const main = palette.neutral.main;
     const primary = palette.primary.main;
-
+  
     const patchLike = async () => {
-        const response = await fetch(`http://localhost:3001/posts/&{postid}/like`, {
-            method: "PATCH",
-            headers: {
-                Authorization: `Bearer ${token}`,
-                "Content-Type" : "application/json",
-            },
-            body: JSON.stringify({ userId: loggedInUserId })
-        });
-        const UpdatedPost = await response.json();
-        dispatch(setPost({ post: UpdatedPost }));
+      const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId: loggedInUserId }),
+      });
+      const updatedPost = await response.json();
+      dispatch(setPost({ post: updatedPost }));
     };
-
+  
     return (
-        <WidgetWrapper margin="2rem 0">
-            <Friend 
-                friendId={postuserId}
-                name={name}
-                subtitle={location}
-                userPicturePath={userPicturePath}
-            />
-            <Typography color={main} sx={{ marginTop: "1rem "}}>
-                {description}
-            </Typography>
-            {picturePath && (
-                <img 
-                    width="100%"
-                    height="auto"
-                    alt="post"
-                    style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-                    src={`http://localhost:3001/assets/${picturePath}`}
-                />
-            )}
-            <FlexBetween marginTop="0.25rem">
-                <FlexBetween gap="1rem">
-                    <FlexBetween gap="0.3rem">
-                        <IconButton onClick={patchLike}>
-                            {isLiked ? (
-                                <FavoriteOutlined sx={{ color: primary }} />
-                            ) : (
-                                <FavoriteBorderOutlined />
-                            )}
-                        </IconButton>
-                        <Typography>{likesCount}</Typography>
-                    </FlexBetween>
-                    <FlexBetween gap="0.3rem" >
-                        <IconButton onClick={()=> setIsComments(!isComments)}>
-                            <ChatBubbleOutlineOutlined />
-                        </IconButton>
-                        <Typography>{comments.length}</Typography>
-                    </FlexBetween>
-                    <IconButton>
-                        <ShareOutlined />
-                        </IconButton>
-                </FlexBetween>
+      <WidgetWrapper m="2rem 0">
+        <Friend
+          friendId={postUserId}
+          name={name}
+          subtitle={location}
+          userPicturePath={userPicturePath}
+        />
+        <Typography color={main} sx={{ mt: "1rem" }}>
+          {description}
+        </Typography>
+        {picturePath && (
+          <img
+            width="100%"
+            height="auto"
+            alt="post"
+            style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
+            src={`http://localhost:3001/assets/${picturePath}`}
+          />
+        )}
+        <FlexBetween mt="0.25rem">
+          <FlexBetween gap="1rem">
+            <FlexBetween gap="0.3rem">
+              <IconButton onClick={patchLike}>
+                {isLiked ? (
+                  <FavoriteOutlined sx={{ color: primary }} />
+                ) : (
+                  <FavoriteBorderOutlined />
+                )}
+              </IconButton>
+              <Typography>{likeCount}</Typography>
             </FlexBetween>
-            {isComments && (
-                <Box marginTop="0.5rem">
-                    {comments.map((comment, i) => (
-                        <Box key={`${name}-${i}`}>
-                            <Divider />
-                            <Typography sx={{ color: main, margin: "0.5rem 0", paddingLeft: '1rem' }}>
-                                {comment}
-                            </Typography>
-                        </Box>
-                    ))}
-                    <Divider />
-                </Box>
-            )}
-        </WidgetWrapper>
+  
+            <FlexBetween gap="0.3rem">
+              <IconButton onClick={() => setIsComments(!isComments)}>
+                <ChatBubbleOutlineOutlined />
+              </IconButton>
+              <Typography>{comments.length}</Typography>
+            </FlexBetween>
+          </FlexBetween>
+  
+          <IconButton>
+            <ShareOutlined />
+          </IconButton>
+        </FlexBetween>
+        {isComments && (
+          <Box mt="0.5rem">
+            {comments.map((comment, i) => (
+              <Box key={`${name}-${i}`}>
+                <Divider />
+                <Typography sx={{ color: main, m: "0.5rem 0", pl: "1rem" }}>
+                  {comment}
+                </Typography>
+              </Box>
+            ))}
+            <Divider />
+          </Box>
+        )}
+      </WidgetWrapper>
     );
-};
-
-export default PostWidget;
+  };
+  
+  export default PostWidget;
+  
